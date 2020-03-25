16013e67483e2dac614c4819825a7f1d6d13f9b8
frugier <hugo.frugier@epitech.eu>
2020-03-25 16:45:17 +0100

    Fix github action - no doxyfile - no test

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MediwatchTest.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void simple()
+        {
+        Assert.False(false, "first great test");
+        }
+    }
+}
