ffe9c8e2517db9f1a0277bb046883bcbf8701b07
alexandre Tahery <alexandre1.tahery@epitech.eu>
2020-08-15 08:37:34 +0900

    Start add request
    Add FormationController

---
+++ Server/Controllers/FormationController.cs
@@ -0,0 +1,18 @@
+using Mediwatch.Shared;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Google;
+
+namespace Mediwatch.Server.Controllers
+{
+    [ApiController]
+    [Route("/[controller]/[action]")]
+    public class FormationController : ControllerBase
+    {
+        
+    }
+}
