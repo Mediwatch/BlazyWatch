ec60002ae014244f0475c573ddfb32d5d987abee
hugo frugier <hugofrugier@rocketmail.com>
2020-09-06 13:37:05 +0900

    Add mock up

---
+++ Client/Pages/FormationTemplate.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+public class FormationTemplate
+{
+	[Required]
+	public string Name { get; set; }
+
+	[Required]
+	public DateTime StartDate { get; set; } = DateTime.Today;
+
+	[Required]
+	public DateTime EndDate { get; set; } = DateTime.Today;
+
+	[Required]
+	public string Location { get; set; }
+
+	[Required]
+	public string Contact { get; set; }
+
+	[Required]
+	public string OrganizationName { get; set; }
+
+	[Required]
+	public decimal Price { get; set; }
+
+	[Required]
+	public string Former { get; set; }
+
+	[Required]
+	public string Description { get; set; }
+
+	[Required]
+	public List<string> Target { get; set; }
+
+	public bool IsRegistered { get; set; }
+}
