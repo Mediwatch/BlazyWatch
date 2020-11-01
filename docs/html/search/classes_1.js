50276c9a5e39de4577efd4be2dfe18ba4807e74e
GitHub Action <action@github.com>
2020-11-01 13:19:41 +0000

    Updtate documentation

--- docs/html/search/classes_1.js
+++ docs/html/search/classes_1.js
@@ -1,8 +1,5 @@
 var searchData=
 [
   ['compagnycontroller',['CompagnyController',['../class_mediwatch_1_1_server_1_1_controllers_1_1_compagny_controller.html',1,'Mediwatch::Server::Controllers']]],
-  ['createidentityschema',['CreateIdentitySchema',['../class_mediwatch_1_1_server_1_1_migrations_1_1_create_identity_schema.html',1,'Mediwatch::Server::Migrations']]],
-  ['createmediwatchdb',['CreateMediwatchDb',['../class_mediwatch_1_1_server_1_1_migrations_1_1_db_context_mediwatch_migrations_1_1_create_mediwatch_db.html',1,'Mediwatch::Server::Migrations::DbContextMediwatchMigrations']]],
-  ['createmediwatchdb1',['CreateMediwatchDb1',['../class_mediwatch_1_1_server_1_1_migrations_1_1_db_context_mediwatch_migrations_1_1_create_mediwatch_db1.html',1,'Mediwatch::Server::Migrations::DbContextMediwatchMigrations']]],
-  ['createmediwatchdb3',['CreateMediwatchDb3',['../class_mediwatch_1_1_server_1_1_migrations_1_1_db_context_mediwatch_migrations_1_1_create_mediwatch_db3.html',1,'Mediwatch::Server::Migrations::DbContextMediwatchMigrations']]]
+  ['createidentityschema',['CreateIdentitySchema',['../class_mediwatch_1_1_server_1_1_migrations_1_1_create_identity_schema.html',1,'Mediwatch::Server::Migrations']]]
 ];
