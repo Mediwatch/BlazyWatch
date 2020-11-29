3f964e1978a28b7fbf71cb24d890c129f248e381
GitHub Action <action@github.com>
2020-11-29 15:49:00 +0000

    Updtate documentation

--- docs/html/search/all_1.js
+++ docs/html/search/all_1.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['compagnycontroller',['CompagnyController',['../class_mediwatch_1_1_server_1_1_controllers_1_1_compagny_controller.html',1,'Mediwatch::Server::Controllers']]],
-  ['createidentityschema',['CreateIdentitySchema',['../class_mediwatch_1_1_server_1_1_migrations_1_1_create_identity_schema.html',1,'Mediwatch::Server::Migrations']]]
+  ['compagnycontroller',['CompagnyController',['../class_mediwatch_1_1_server_1_1_controllers_1_1_compagny_controller.html',1,'Mediwatch::Server::Controllers']]]
 ];
