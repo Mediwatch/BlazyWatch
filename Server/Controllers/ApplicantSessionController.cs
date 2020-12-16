656a4a6fad89413aa89605845bc655edf5d33b5e
alexandre Tahery <alexandre1.tahery@epitech.eu>
2020-12-16 16:06:34 +0900

    ApplicantSessionController put patched

--- Server/Controllers/ApplicantSessionController.cs
+++ Server/Controllers/ApplicantSessionController.cs
@@ -57,7 +57,7 @@ namespace Mediwatch.Server.Controllers
             {
                 throw;
             }
-            return NoContent();
+            return StatusCode(200);
         }
 
         // POST: /ApplicantSession
