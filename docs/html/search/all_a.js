395087e691f4c1d71c378877092a15254ff11a0a
GitHub Action <action@github.com>
2020-12-26 09:31:28 +0000

    Updtate documentation

--- docs/html/search/all_a.js
+++ docs/html/search/all_a.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['userrole',['UserRole',['../class_mediwatch_1_1_server_1_1_migrations_1_1_identity_data_1_1_user_role.html',1,'Mediwatch::Server::Migrations::IdentityData']]],
   ['userscontroller',['UsersController',['../class_mediwatch_1_1_server_1_1_controllers_1_1_users_controller.html',1,'Mediwatch::Server::Controllers']]]
 ];
