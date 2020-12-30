720e269f4e67819a68063bcd31df12907853097f
alexandre Tahery <alexandre1.tahery@epitech.eu>
2020-12-31 01:12:06 +0900

    OrderInfo inco

---
+++ Shared/Models/OrderInfo.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+
+namespace Mediwatch.Shared.Models
+{
+    public class OrderInfo
+    {
+        public string id {get; set;}
+        public string email_customer {get; set;}
+        public string email_seller {get; set;}
+        public string name_customer {get; set;}
+        public float price_order {get; set;}
+
+    }
+}
