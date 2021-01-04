8b49edbab4c4e1268aaefcb5907a5abc2dab2f89
GitHub Action <action@github.com>
2021-01-04 06:14:57 +0000

    Update documentation

--- docs/html/search/all_7.js
+++ docs/html/search/all_7.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['identitydatacontext',['IdentityDataContext',['../class_mediwatch_1_1_server_1_1_areas_1_1_identity_1_1_data_1_1_identity_data_context.html',1,'Mediwatch::Server::Areas::Identity::Data']]],
   ['identitydatacontextmodelsnapshot',['IdentityDataContextModelSnapshot',['../class_mediwatch_1_1_server_1_1_migrations_1_1_identity_data_1_1_identity_data_context_model_snapshot.html',1,'Mediwatch::Server::Migrations::IdentityData']]],
-  ['identityhostingstartup',['IdentityHostingStartup',['../class_mediwatch_1_1_server_1_1_areas_1_1_identity_1_1_identity_hosting_startup.html',1,'Mediwatch::Server::Areas::Identity']]]
+  ['identityhostingstartup',['IdentityHostingStartup',['../class_mediwatch_1_1_server_1_1_areas_1_1_identity_1_1_identity_hosting_startup.html',1,'Mediwatch::Server::Areas::Identity']]],
+  ['invoicegenerator',['InvoiceGenerator',['../class_invoice_generator.html',1,'']]]
 ];
