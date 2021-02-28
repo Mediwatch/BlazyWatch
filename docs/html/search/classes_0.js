cfe32c2b875e3f665d85f5d99923aad2a27fc75e
GitHub Action <action@github.com>
2021-02-28 02:48:58 +0000

    Update documentation

--- docs/html/search/classes_0.js
+++ docs/html/search/classes_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['accountcontroller',['AccountController',['../class_mediwatch_1_1_server_1_1_controllers_1_1_account_controller.html',1,'Mediwatch::Server::Controllers']]],
-  ['applicantsessioncontroller',['ApplicantSessionController',['../class_mediwatch_1_1_server_1_1_controllers_1_1_applicant_session_controller.html',1,'Mediwatch::Server::Controllers']]],
-  ['articlescontroller',['ArticlesController',['../class_blazing_blog_1_1_server_1_1_controllers_1_1_articles_controller.html',1,'BlazingBlog::Server::Controllers']]]
+  ['accountcontroller_62',['AccountController',['../class_mediwatch_1_1_server_1_1_controllers_1_1_account_controller.html',1,'Mediwatch::Server::Controllers']]],
+  ['applicantsessioncontroller_63',['ApplicantSessionController',['../class_mediwatch_1_1_server_1_1_controllers_1_1_applicant_session_controller.html',1,'Mediwatch::Server::Controllers']]],
+  ['articlescontroller_64',['ArticlesController',['../class_blazing_blog_1_1_server_1_1_controllers_1_1_articles_controller.html',1,'BlazingBlog::Server::Controllers']]]
 ];
--- docs/html/search/namespaces_0.js
+++ docs/html/search/namespaces_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['blazingblog',['BlazingBlog',['../namespace_blazing_blog.html',1,'']]],
-  ['controllers',['Controllers',['../namespace_blazing_blog_1_1_server_1_1_controllers.html',1,'BlazingBlog::Server']]],
-  ['server',['Server',['../namespace_blazing_blog_1_1_server.html',1,'BlazingBlog']]]
+  ['blazingblog_83',['BlazingBlog',['../namespace_blazing_blog.html',1,'']]],
+  ['controllers_84',['Controllers',['../namespace_blazing_blog_1_1_server_1_1_controllers.html',1,'BlazingBlog::Server']]],
+  ['server_85',['Server',['../namespace_blazing_blog_1_1_server.html',1,'BlazingBlog']]]
 ];
