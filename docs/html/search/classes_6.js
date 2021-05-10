ba40849a9cfdbe4f7d103173ec7a986f985b2b11
GitHub Action <action@github.com>
2021-05-10 14:00:22 +0000

    Update documentation

--- docs/html/search/classes_6.js
+++ docs/html/search/classes_6.js
@@ -3,7 +3,7 @@ var searchData=
   ['identitydatacontext_84',['IdentityDataContext',['../class_mediwatch_1_1_server_1_1_areas_1_1_identity_1_1_data_1_1_identity_data_context.html',1,'Mediwatch::Server::Areas::Identity::Data']]],
   ['identitydatacontextmodelsnapshot_85',['IdentityDataContextModelSnapshot',['../class_mediwatch_1_1_server_1_1_migrations_1_1_identity_data_context_model_snapshot.html',1,'Mediwatch::Server::Migrations']]],
   ['identityhostingstartup_86',['IdentityHostingStartup',['../class_mediwatch_1_1_server_1_1_areas_1_1_identity_1_1_identity_hosting_startup.html',1,'Mediwatch::Server::Areas::Identity']]],
-  ['innitialcreate_87',['innitialCreate',['../class_mediwatch_1_1_server_1_1_migrations_1_1innitial_create.html',1,'Mediwatch.Server.Migrations.innitialCreate'],['../class_mediwatch_1_1_server_1_1_migrations_1_1_db_context_mediwatch_migrations_1_1innitial_create.html',1,'Mediwatch.Server.Migrations.DbContextMediwatchMigrations.innitialCreate']]],
+  ['innitialcreate_87',['innitialCreate',['../class_mediwatch_1_1_server_1_1_migrations_1_1_db_context_mediwatch_migrations_1_1innitial_create.html',1,'Mediwatch.Server.Migrations.DbContextMediwatchMigrations.innitialCreate'],['../class_mediwatch_1_1_server_1_1_migrations_1_1innitial_create.html',1,'Mediwatch.Server.Migrations.innitialCreate']]],
   ['invoicearchiver_88',['InvoiceArchiver',['../class_invoice_archiver.html',1,'']]],
   ['invoicegenerator_89',['InvoiceGenerator',['../class_invoice_generator.html',1,'']]]
 ];
