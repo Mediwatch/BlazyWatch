9ae8152a6120a36afddbe99649e27bac3d4cc42e
GitHub Action <action@github.com>
2021-10-11 20:21:54 +0000

    Update documentation

--- docs/html/search/classes_d.js
+++ docs/html/search/classes_d.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['smartbuttonhttpresponse_186',['SmartButtonHttpResponse',['../class_mediwatch_1_1_server_1_1_pay_pal_1_1_smart_button_http_response.html',1,'Mediwatch::Server::PayPal']]],
-  ['startup_187',['Startup',['../class_mediwatch_1_1_server_1_1_startup.html',1,'Mediwatch::Server']]]
+  ['smartbuttonhttpresponse_188',['SmartButtonHttpResponse',['../class_mediwatch_1_1_server_1_1_pay_pal_1_1_smart_button_http_response.html',1,'Mediwatch::Server::PayPal']]],
+  ['startup_189',['Startup',['../class_mediwatch_1_1_server_1_1_startup.html',1,'Mediwatch::Server']]]
 ];
--- docs/html/search/namespaces_2.js
+++ docs/html/search/namespaces_2.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['controllers_219',['Controllers',['../namespace_server_1_1_controllers.html',1,'Server']]],
-  ['interface_220',['Interface',['../namespace_server_1_1_services_1_1_interface.html',1,'Server::Services']]],
-  ['server_221',['Server',['../namespace_server.html',1,'']]],
-  ['services_222',['Services',['../namespace_server_1_1_services.html',1,'Server']]],
-  ['utils_223',['Utils',['../namespace_server_1_1_utils.html',1,'Server']]]
+  ['controllers_221',['Controllers',['../namespace_server_1_1_controllers.html',1,'Server']]],
+  ['interface_222',['Interface',['../namespace_server_1_1_services_1_1_interface.html',1,'Server::Services']]],
+  ['server_223',['Server',['../namespace_server.html',1,'']]],
+  ['services_224',['Services',['../namespace_server_1_1_services.html',1,'Server']]],
+  ['utils_225',['Utils',['../namespace_server_1_1_utils.html',1,'Server']]]
 ];
