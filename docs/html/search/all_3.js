b6bbdcbc962b13f473e8cd81362326f5d6cc8cac
GitHub Action <action@github.com>
2021-10-20 19:49:20 +0000

    Update documentation

--- docs/html/search/all_3.js
+++ docs/html/search/all_3.js
@@ -3,7 +3,7 @@ var searchData=
   ['dbcontextmediwatch_30',['DbContextMediwatch',['../class_server_1_1_db_context_mediwatch.html',1,'Server']]],
   ['dbcontextmediwatchmodelsnapshot_31',['DbContextMediwatchModelSnapshot',['../class_mediwatch_1_1_server_1_1_migrations_1_1_db_context_mediwatch_migrations_1_1_db_context_mediwatch_model_snapshot.html',1,'Mediwatch::Server::Migrations::DbContextMediwatchMigrations']]],
   ['deleteapplicantsession_32',['DeleteApplicantSession',['../class_mediwatch_1_1_server_1_1_controllers_1_1_applicant_session_controller.html#ad88d09ad6ea3e44ec9c19551694ae4a6',1,'Mediwatch::Server::Controllers::ApplicantSessionController']]],
-  ['deleteformation_33',['DeleteFormation',['../class_mediwatch_1_1_server_1_1_controllers_1_1_formation_controller.html#af3e300009a2f7d0d1551da387effbe09',1,'Mediwatch::Server::Controllers::FormationController']]],
+  ['deleteformation_33',['DeleteFormation',['../class_mediwatch_1_1_server_1_1_controllers_1_1_formation_controller.html#a15ecebde3972ba50988768d0fb5699a2',1,'Mediwatch::Server::Controllers::FormationController']]],
   ['deletejointagform_34',['DeleteJoinTagForm',['../class_mediwatch_1_1_server_1_1_controllers_1_1_join_tag_formation_controller.html#aeea9e3e94ca7514cf9d927cadc5ccc72',1,'Mediwatch::Server::Controllers::JoinTagFormationController']]],
   ['deletetodoitem_35',['DeleteTodoItem',['../class_mediwatch_1_1_server_1_1_controllers_1_1_compagny_controller.html#a5c33b6687ecca9c5b060f029ce809e7d',1,'Mediwatch::Server::Controllers::CompagnyController']]],
   ['deleteuserformation_36',['DeleteUserFormation',['../class_mediwatch_1_1_server_1_1_controllers_1_1_users_controller.html#a03952b3be592e56eb93c7c6abdfe297b',1,'Mediwatch::Server::Controllers::UsersController']]]
