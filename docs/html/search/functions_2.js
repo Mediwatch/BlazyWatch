681852c8d4be3860077afb4279b46af45a60c525
GitHub Action <action@github.com>
2021-11-21 21:17:07 +0000

    Update documentation

--- docs/html/search/functions_2.js
+++ docs/html/search/functions_2.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['cancel_230',['Cancel',['../class_mediwatch_1_1_server_1_1_controllers_1_1_checkout_controller.html#a2af05dc58e8cc3fa0c8296ed67d85c88',1,'Mediwatch::Server::Controllers::CheckoutController']]],
-  ['completeasync_231',['CompleteAsync',['../class_mediwatch_1_1_server_1_1_controllers_1_1_checkout_controller.html#ae98f3a1b0041d703f93485d943893550',1,'Mediwatch::Server::Controllers::CheckoutController']]],
+  ['completeasync_231',['CompleteAsync',['../class_mediwatch_1_1_server_1_1_controllers_1_1_checkout_controller.html#a6e441834b83f92691e34c91932bcc0d6',1,'Mediwatch::Server::Controllers::CheckoutController']]],
   ['create_232',['Create',['../class_mediwatch_1_1_server_1_1_controllers_1_1_checkout_controller.html#a76be1fec719f0643eb5c207b3b696dcf',1,'Mediwatch::Server::Controllers::CheckoutController']]],
   ['createarticle_233',['CreateArticle',['../class_blazing_blog_1_1_server_1_1_controllers_1_1_articles_controller.html#a9569853c2a94646215f6bb863cb07be2',1,'BlazingBlog::Server::Controllers::ArticlesController']]]
 ];
